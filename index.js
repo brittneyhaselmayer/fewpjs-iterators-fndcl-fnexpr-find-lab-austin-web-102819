c5b33be0c5c0b75582fcfffb0ae31e58e45cf39c
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-12-26 15:41:45 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+// function superbowlWin(record){
+//   record.find(function(record){
+//     if record.result === "W"
+//     return record.year
+
+//     else 
+//     return undefined
+//   })
+// }
+
+
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined}
